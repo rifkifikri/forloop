55fc29d91e02653e0e0427de06339b17f439ed58
rifkifikri <rifkifikri000@gmail.com>
2021-08-05 23:10:12 +0700

    looping

---
+++ looping.php
@@ -0,0 +1,13 @@
+<?php
+    $bts=5;
+
+    //cara 1
+    //php yang di dalamnya terdapat script html
+    //untuk menerapkan tag html maka php harus di akhiri terlebih dahulu baru dibuka kembali setelah tag html
+    for ($i=0;$i<=$bts;$i++){
+        ?>
+        <h1>Hallo</h1> 
+        <?php
+    }
+
+?>
